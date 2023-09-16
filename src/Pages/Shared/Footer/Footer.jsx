fcbbc66350f9c01e228f78203a83a0704be8f700
NazmulAyyan <rnazmul2424@gmail.com>
2023-09-17 01:42:55 +0600

    first commit

---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div>
+            <h3 className="text-4xl">this is footer</h3>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <h3 className="text-4xl">this is navbar</h3>
+        </div>
+    );
+};
+
+export default Navbar;
