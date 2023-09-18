7618044b79b929e877f51df350dca4ba29499206
Sihab Hossain <sihabhossain380@gmail.com>
2023-09-18 14:49:25 +0600

    contact page banner added

--- src/LayOut/Main.jsx
+++ src/LayOut/Main.jsx
@@ -3,13 +3,13 @@ import Navbar from "../Pages/Shared/Navbar/Navbar";
 import Footer from "../Pages/Shared/Footer/Footer";
 
 const Main = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,18 +1,21 @@
-import {
-    createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../LayOut/Main";
 import Home from "../Pages/Home/Home";
+import Contact from "../Pages/Contact/Contact";
 
 export const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
         path: "/",
-        element: <Main></Main>,
-        children: [
-            {
-                path: "/",
-                element: <Home></Home>,
-            }
-        ]
-    },
+        element: <Home></Home>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+    ],
+  },
 ]);
