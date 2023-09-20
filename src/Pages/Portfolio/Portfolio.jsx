5c1a468d15f47f0878075a7bd69941c98961cea0
Sihab Hossain <sihabhossain380@gmail.com>
2023-09-20 19:51:32 +0600

    banner changed on portfolio page

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,14 +1,17 @@
-import Banner from "../Shared/Banner/Banner";
 import PortfolioCard from "./PortfolioCard/PortfolioCard";
 import { PortfoliDAta } from "./PortfolioData/PortFolioData";
 import "./Portfolio.css";
 import { Link } from "react-router-dom";
+import TitleCover from "../../Components/TitleCover/TitleCover";
 
 const Portfolio = () => {
   return (
     <section className=" overflow-auto">
       {/* banner section */}
-      <Banner title={"Portfolio standard"} />
+      <TitleCover
+        serviceText="Our Portfolio"
+        pageTitle="Our Portfolio"
+      ></TitleCover>
       {/* card section */}
       <div className=" md:grid lg:grid-cols-3 md:grid-cols-2 gap-8 w-10/12 mx-auto py-[150px]">
         {PortfoliDAta?.map((CardData, index) => (
